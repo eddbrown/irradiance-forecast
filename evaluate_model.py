574672b1ac01e106cbec9fdf52d33d1efe7f0d09
Edward Brown <edward.j.e.brown@gmail.com>
2023-03-06 13:27:55 +0000

    Clean up persistence model evaluation

--- evaluate_model.py
+++ evaluate_model.py
@@ -9,6 +9,7 @@ def evaluate_model(model, dataset, batch_size=32, device='cuda:0', num_workers=8
     dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, drop_last=False, shuffle=False)
     predictions = []
     targets = []
+    persistences = []
     losses = []
     
     with torch.no_grad():
@@ -17,26 +18,30 @@ def evaluate_model(model, dataset, batch_size=32, device='cuda:0', num_workers=8
             model_output = model.forward(image.to(device), persistence.to(device))
             predictions.append(model_output.detach().cpu().numpy())
             targets.append(target.detach().cpu().numpy())
+            persistences.append(persistence)
     
     predictions = np.concatenate(predictions)
     targets = np.concatenate(targets)
+    persistence = np.concatenate(persistences)
     loss = mse(predictions, targets)
     
     predictions = dataset.scaler.inverse_transform(predictions)
+    targets = dataset.scaler.inverse_transform(targets)
+    persistences = dataset.scaler.inverse_transform(persistences)
+    
     results = {}
     for band_index, band in enumerate(dataset.irradiance_data.columns):
         results['loss'] = loss
-        persistence_prediction = dataset.irradiance_data[band].shift(freq=f'{60*dataset.forecast_horizon_hours}T')
-        persistence_mape =  100*np.abs((persistence_prediction - dataset.irradiance_data[band]).div(dataset.irradiance_data[band])).dropna().mean()
-       
-        model_mape = mape(predictions[:, band_index], dataset.irradiance_data[band])
+        persistence_mape =  mape(persistences, targets)
+        model_mape = mape(predictions, targets)
         
         if persistence_mape > 0:
             persistence_score = 1.0 - (model_mape/persistence_mape)
         else:
             persistence_score = 0.0
-        results[f'rmse_{band}'] = rmse(predictions[:, band_index], dataset.irradiance_data[band])
-        results[f'mape_{band}'] = mape(predictions[:, band_index], dataset.irradiance_data[band])
+            
+        results[f'rmse_{band}'] = rmse(predictions, targets)
+        results[f'mape_{band}'] = mape(predictions, targets)
         results[f'persistence_mape_{band}'] = persistence_mape
         results[f'persistence_score_{band}'] = persistence_score
     
