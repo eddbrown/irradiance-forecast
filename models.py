9735cbbf8ddf7ff93773e7863275d8496faa5fce
Edward Brown <edward.j.e.brown@gmail.com>
2023-03-19 12:04:15 +0000

    Add Max Vit

--- models.py
+++ models.py
@@ -64,3 +64,37 @@ class IrradianceRegressorWithPersistence(nn.Module):
             features = self.pretrained_model.forward_features(three_channel)[:,-1,:]
         fc_output = self.fc(torch.cat((features, persistence),dim=1))
         return fc_output
+
+class MaxVit(nn.Module):
+    def __init__(self, hidden_layer_size=100, output_size=23):
+        super(MaxVit, self).__init__()
+        self.image_dims = 224
+        self.hidden_layer_size = hidden_layer_size
+        self.resize = nn.Upsample(self.image_dims)
+        self.pretrained_model = timm.create_model('maxvit_base_tf_224.in21k')
+        self.output_size = output_size
+        self.fc = nn.Sequential(
+            nn.Linear(1000+23, hidden_layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_layer_size, hidden_layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_layer_size, hidden_layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_layer_size, hidden_layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_layer_size, self.output_size),
+            nn.LeakyReLU(),
+        )
+
+    def forward(self, images, persistence):
+        batch_size = images.shape[0]
+        resized_image = self.resize(images)
+        # If three channel already, just run the model, else concatenate three times.
+        if images.shape[1] == 3:
+            features = self.pretrained_model.forward(resized_image)
+        else:
+            three_channel = torch.cat([resized_image, resized_image, resized_image], dim=1)
+            features = self.pretrained_model.forward(three_channel)
+        print(features.shape, persistence.shape)
+        fc_output = self.fc(torch.cat((features, persistence),dim=1))
+        return fc_output
